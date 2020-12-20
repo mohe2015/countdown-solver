efd480f7a464969a1385020020120e3ef84553f4
mohe2015 <Moritz.Hedtke@t-online.de>
2020-12-20 21:05:57 +0100

    remove code that's not needed

--- src/main.rs
+++ src/main.rs
@@ -108,66 +108,6 @@ fn generate_numbers() {
     println!("{}", counter);
 }
 
-struct OperatorCombinationIterator {
-    combination: u32
-}
-
-impl Iterator for OperatorCombinationIterator {
-    type Item = u32;
-
-    fn next(&mut self) -> Option<u32> {
-        // maybe do it the over way around? (bit representation)
-        // lowest 16 bits are lowest branches
-        // then 8, 4, 2, and 1 is the top
-
-        // shift right:
-        // 31 [30] [29 28] [27 26 25 24] [23 22 21 20 19 18 17 16] [15 14 13 12 11 10 9 8 7 6 5 4 3 2 1 0]
-        // count leaves:
-        // 30 && !29 + 30 && !28
-        // 29 & !27 + 29 & !26
-        // 15 + ... + 0
-
-        // top down should be more efficient?
-        
-        'outer: loop {
-            if self.combination & (1 << 31) != 0 {
-                return None;
-            }
-
-            let mut leaves: u32 = 0;
-            
-            let combination_complement = !self.combination;
-            for i in 0..30 {
-                let a = (combination_complement >> i);
-                let b = (self.combination >> (i/2+16));
-                leaves += (a & b) & 1;
-                let invalid = (a | b) & 1 == 0;
-                if invalid || leaves > 6 {
-                    self.combination += 1;
-                    continue 'outer;
-                }
-            }
-
-            self.combination += 1;
-            break;
-        }
-
-        Some(self.combination)
-    }
-}
-
-fn generate_operator_combinations() {
-    let operator_combinations = OperatorCombinationIterator {
-        combination: 0
-    };
-    let mut counter: u64 = 0;
-    for combination in operator_combinations {
-        counter += u64::from(combination); 
-        //println!("{}", combination);
-    }
-    println!("{}", counter);
-}
-
 fn generate_operator_orders() {
     // six numbers
     // allowed ops: + - * /
@@ -204,28 +144,6 @@ fn main() {
             println!("{}", i+100)
         }
     }
-}
-
-fn alternative_generate_operator_combinations() {
-    let mut counter: u64 = 0;
-    'outer: for combination in 0u64..(1 << 31) {
-
-        let mut leaves: u64 = 0;
-        let mut valid: u64 = 1;
-        
-        let combination_complement = !combination;
-        for i in 0..30 {
-            let a = (combination_complement >> i);
-            let b = (combination >> (i/2+16));
-            leaves += (a & b) & 1;
-            valid &= a | b;
-        }
-        if (valid & 1) == 0 || leaves > 6 {
-            continue 'outer;
-        }
-        counter += 1;
-    }
-    println!("{}", counter);
 }
 
 // log2(100*75*50*25*10*10) = 30
