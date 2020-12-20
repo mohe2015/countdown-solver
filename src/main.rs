67bf50d368bd7b10717f4a20d493229015b91daf
mohe2015 <Moritz.Hedtke@t-online.de>
2020-12-20 21:08:26 +0100

    more cleanup

--- src/main.rs
+++ src/main.rs
@@ -40,59 +40,7 @@ impl Iterator for CountdownNumberIterator {
         let mut result = MAJOR_NUMBERS[big_number_permutation]; // TODO FIXME does this break the original array contentes?
         let length = LENGTH[big_number_permutation];
 
-        /*
-        // this would be the more efficient solution but it has problems
-        let mut index = 0;
-        for i in (0..20).step_by(2) {
-            let state = (self.permutation >> i) & 0b11;
-            if state == 2 {
-                result[index] = i/2+1;
-                index += 1;
-                // TODO FIXME this could overflow
-                result[index] = i/2+1;
-                index += 1;
-            }
-            if state == 1 {
-                result[index] = i/2+1;
-                index += 1;
-            }
-        }
-        // 2 bits per small number (one invalid state)
-        // for every number from 1 - 10 we can choose (), (x), (x x) - reverse doesnt matter
-        // so choose one of these until you got the amount of numbers needed
-        */
-       
-        // small numbers
-        // 2-5 numbers needed (maybe specialize for every one of these)
-
-        // more stupid and less efficident but easier implementation
-        // TODO FIXME this creates lots of duplicates as the bits at the end that aren't used are changed only
-        loop {
-            if self.permutation >= (1 << 26) {
-                return None;
-            }
-
-            let mut index = 0;
-            let mut add: u32 = 4;
-            for i in 2..22 {
-                if ((self.permutation >> (23-i)) & 1) == 1 {
-                    result[index] = i/2;
-                    index += 1;
-                }
-                if index == length { // fully filled free places
-                    add = 0;
-                    self.permutation = (self.permutation >> (23-i)) << (23-i);
-                    self.permutation += 1 << (24-i);
-                    break;
-                }
-            }
-            self.permutation += add; // lowest bits not used in loop
-            if index == length { // otherwise not enough numbers where used (0s left)
-                break;
-            }
-        }
-        
-        Some(result)
+        None
     }
 }
 
@@ -108,39 +56,13 @@ fn generate_numbers() {
     println!("{}", counter);
 }
 
-fn generate_operator_orders() {
-    // six numbers
-    // allowed ops: + - * /
-    // order choosen freely
-    // not all numbers need to be used but none can be used twice
-    // division only without remainder
-    // only positive numbers at any stage
-    // + and * is commutative
-    // / and - can only be used rarely properly (underflow, remainder)
-    
-    // starting with
-    // a + b
-    // a - b
-    // a * b
-    // a / b
-    // b - a
-    // b / a
-
-    // YES: maybe fixed number parameter order? maybe also generate b + c etc.?
-    // probably something recursive? (write comparison function?)
-    // in theory a valid a - b would exclude b - a but I think this is too complicated
-
-    // maybe sort them so its easy to detect commutativity
-    // maybe memoization helps
-}
-
 fn main() {
     //println!("Hello, world!");
     //generate_numbers()
     let mut solutions = [false; 900];
     step(&mut solutions, [1, 3, 5, 25, 50, 100]);
     for (i, solution) in solutions.iter().enumerate() {
-        if *solution {
+        if !*solution {
             println!("{}", i+100)
         }
     }
